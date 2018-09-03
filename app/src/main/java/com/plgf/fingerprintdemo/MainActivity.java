92d01c4057fbc6c88e612260ef5b8b8748c0ed74
PilotDeveloperAccount <1038883524@qq.com>
2018-09-03 17:04:55 +0800

    优化

--- app/src/main/java/com/plgf/fingerprintdemo/MainActivity.java
+++ app/src/main/java/com/plgf/fingerprintdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.plgf.fingerprintdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.plgf.fingerprint.CheckFinger;
 import com.plgf.fingerprint.FingerprintUtils;
@@ -23,5 +24,10 @@ public class MainActivity extends AppCompatActivity implements CheckFinger {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onOtherAuthenticated() {
+        Toast.makeText(this, "请使用其他认证方式", Toast.LENGTH_SHORT).show();
     }
 }
